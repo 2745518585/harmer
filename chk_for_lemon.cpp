816d16ebeac585da32042415e3f1a56bc829ad61
2745518585 <zz2745518585@gmail.com>
2024-01-12 15:43:42 +0800

    update

---
+++ chk_for_lemon.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+#include"testlib_for_lemon.h"
+using namespace std;
+template<typename S> inline char gc(S &st)
+{
+    return st.eof()?EOF:st.readChar();
+}
+template<typename T,typename S> inline void read(T &x,S &st)
+{
+    T u=1,t=0;char c=gc(st);
+    while(c<'0'||c>'9') {if(c==EOF) return x=0,void(); if(c=='-') u=-1; c=gc(st);}
+    while(c>='0'&&c<='9') t*=10,t+=c-'0',c=gc(st);
+    x=u*t;return;
+}
+const int N=1001;
+int n,a[N],b[N];
+int main(int argc,char **argv)
+{
+    registerLemonChecker(argc,argv);
+    read(n,inf);
+    for(int i=1;i<=n;++i) read(a[i],inf);
+    for(int i=1;i<=n;++i) read(b[i],inf);
+    string p1=ans.readToken(),p2=ouf.readToken();
+    if(p1!=p2)
+    {
+        quitf(_wa,"Wrong Answer");
+    }
+    if(p1=="NO") quitf(_ok,"Accepted");
+    bool u=true;
+    int m;
+    read(m,ouf);
+    for(int l=1;l<=m;++l)
+    {
+        int x,y,w=0;
+        read(x,ouf),read(y,ouf);
+        if(x<1||x>n||y<1||y>n) u=false;
+        if(x<y)
+        {
+            for(int i=x+1;i<=y;++i) w+=(a[x]<a[i]?1:-1);
+            if(w!=0) u=false;
+            for(int i=x+1;i<=y;++i) swap(a[i-1],a[i]);
+        }
+        else
+        {
+            for(int i=x-1;i>=y;--i) w+=(a[x]<a[i]?1:-1);
+            if(w!=0) u=false;
+            for(int i=x-1;i>=y;--i) swap(a[i+1],a[i]);
+        }
+    }
+    for(int i=1;i<=n;++i)
+    {
+        if(a[i]!=b[i]) u=false;
+    }
+    if(u) quitf(_ok,"Accepted");
+    else quitp(perfectScore*0.2,"invaild operation");
+    return 0;
+}
